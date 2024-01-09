6b1fd1eebff946cdb60ccab39695736939d01049
NikWhit <135679785+NikWhit@users.noreply.github.com>
2024-01-08 17:03:36 -0700

    checkpoint in building models

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+    title: {
+      type: DataTypes.STRING,
+    },
+    post: {
+      type: DataTypes.TEXT,
+    },
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Project;
+
+// Credit goes to the person who made the code to the 28-MiniProject for MVC
--- models/index.js
+++ models/index.js
@@ -3,7 +3,38 @@ const User = require('./User');
 const Post = require('./Post');
 const Comments = require('./Comment');
 
+//Post owned by user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+//User has many Posts
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
+//Commends belonging to the user
+Comments.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+//User many comments
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//comments to posts
+Comments.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+//post to many comments
+Post.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comments };
 
 
 
